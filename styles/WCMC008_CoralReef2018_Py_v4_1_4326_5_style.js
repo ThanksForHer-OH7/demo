a94ec473749f24b79719079b67aa43d2dd71b130
ThanksForHer-OH7 <117881359+ThanksForHer-OH7@users.noreply.github.com>
2022-11-10 13:50:51 +1100

    Add files via upload

---
+++ styles/WCMC008_CoralReef2018_Py_v4_1_4326_5_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_WCMC008_CoralReef2018_Py_v4_1_4326_5 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    var style = [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(182,182,182,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
---
+++ styles/featurespoint_4_style.js
@@ -0,0 +1,30 @@
+var size = 0;
+var placement = 'point';
+
+var style_featurespoint_4 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    ''
+    var labelText = "";
+    size = 0;
+    var labelFont = "7.800000000000001px \'Open Sans\', sans-serif";
+    var labelFill = "#013257";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 0.6000000000000001;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("name") !== null) {
+        labelText = String(feature.get("name"));
+    }
+    
+    var style = [ new ol.style.Style({
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor, bufferWidth)
+    })];;
+
+    return style;
+};
